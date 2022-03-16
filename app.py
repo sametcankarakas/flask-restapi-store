c5e5a92241b091037ae8fc092577f1bfb5ce7e8c
Samet Can KARAKAŞ <scankarakas@hotmail.com>
2022-03-16 17:28:26 +0300

    env variable name changed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from models.user import UserModel
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'sam'
 api = Api(app)
